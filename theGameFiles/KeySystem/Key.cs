75e9a4c4c2fa84f629186e55d3e42307bbedcc2b
lightninga98 <134561838+lightninga98@users.noreply.github.com>
2023-07-09 19:18:41 -0500

    Created Key and Lock
    Created the Key and Lock Classes.
    No demo program currently

---
+++ theGameFiles/KeySystem/Key.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace theGameFiles.KeySystem
+{
+    internal class Key
+    {
+        public enum KeyType //Can name these whatever
+        {
+            A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z
+        }
+
+        private readonly KeyType type;
+        private readonly string png_name;
+        public Key(KeyType type, string png_name)
+        {
+            this.type = type;
+            this.png_name = png_name;
+        }
+        public KeyType GetKeyType()
+            { return type; }
+        public string GetPngName() 
+            { return png_name;}
+    }
+}
--- theGameFiles/Program.cs
+++ theGameFiles/Program.cs
@@ -9,17 +9,3 @@ using System.IO;
 using System.Collections;
 using theGameFiles.MapBuilding;
 
-
-Location goblinHead = new(0, 0, "goblin", 4, 6);
-Location defaultPng = new();
-Location[] locations = new[] { defaultPng, goblinHead };
-MapData mapHolder = new("testLoad", locations);
-mapHolder.SaveMapData();
-MapData mapHolderTwo = new();
-mapHolderTwo.LoadMapData("testLoad");
-Location[] printLocations = mapHolder.GetLocationArray();
-Console.Write(printLocations.Length);
-foreach(Location location in printLocations)
-{
-    Console.Write("\n" + location.ToString());
-}
